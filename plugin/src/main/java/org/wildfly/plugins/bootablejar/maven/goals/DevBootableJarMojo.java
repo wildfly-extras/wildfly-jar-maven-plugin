449ef6219c7b866e70c7b0717044eac244aa3267
Doychin Bondzhev <doychin@dsoft-bg.com>
2020-08-19 19:22:24 +0300

    Use double backslash instead of unix separator when on Windows.
    Signed-off-by: Doychin Bondzhev <doychin@dsoft-bg.com>

--- plugin/src/main/java/org/wildfly/plugins/bootablejar/maven/goals/DevBootableJarMojo.java
+++ plugin/src/main/java/org/wildfly/plugins/bootablejar/maven/goals/DevBootableJarMojo.java
@@ -19,6 +19,8 @@ package org.wildfly.plugins.bootablejar.maven.goals;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.regex.Matcher;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -64,7 +66,7 @@ public final class DevBootableJarMojo extends AbstractBuildBootableJarMojo {
     }
 
     private void configureScanner(Path deployments, List<String> commands) {
-        String deploymentPath = deployments.toString().replaceAll("\\x5c", "/");
+        String deploymentPath = deployments.toString().replaceAll("\\\\", "\\\\\\\\");
         commands.add("/subsystem=deployment-scanner/scanner=" + DEPLOYMENT_SCANNER_NAME + ":add(scan-interval=1000,auto-deploy-exploded=false,"
                 + "path=\"" + deploymentPath + "\")");
     }
