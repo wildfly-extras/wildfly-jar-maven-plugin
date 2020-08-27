d8e5f0e7098319e7f3bbf45c248227f04de75e76
JF Denise <jdenise@redhat.com>
2020-08-27 10:54:29 +0200

    Fix for WFWIP-329, Disable management console in mgmt http-interface

--- plugin/src/main/java/org/wildfly/plugins/bootablejar/maven/cloud/CloudConfig.java
+++ plugin/src/main/java/org/wildfly/plugins/bootablejar/maven/cloud/CloudConfig.java
@@ -97,6 +97,13 @@ public class CloudConfig {
     }
 
     public void addCLICommands(BuildBootableJarMojo mojo, List<String> commands) throws Exception {
+        try (InputStream stream = CloudConfig.class.getResourceAsStream("openshift-management-script.cli")) {
+            List<String> lines
+                    = new BufferedReader(new InputStreamReader(stream,
+                            StandardCharsets.UTF_8)).lines().collect(Collectors.toList());
+            commands.addAll(lines);
+        }
+
         try (InputStream stream = CloudConfig.class.getResourceAsStream("openshift-logging-script.cli")) {
             List<String> lines
                     = new BufferedReader(new InputStreamReader(stream,
