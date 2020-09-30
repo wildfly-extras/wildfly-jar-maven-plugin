a5db6f6def9e0b15c6155e609c47e5f789e4ea84
Yeray Borges <legobrunch@gmail.com>
2020-09-30 09:47:19 +0100

    Web Console Bootable Jar example

---
+++ examples/web-console/src/main/java/org/wildfly/plugins/demo/web/console/HelloWorldEndpoint.java
@@ -0,0 +1,17 @@
+package org.wildfly.plugins.demo.web.console;
+
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+
+@Path("/hello")
+public class HelloWorldEndpoint {
+    @GET
+    @Produces("text/plain")
+    public Response doGet() {
+        return Response.ok("Hello from WildFly bootable jar!").build();
+    }
+}
---
+++ examples/web-console/src/main/java/org/wildfly/plugins/demo/web/console/RestApplication.java
@@ -0,0 +1,8 @@
+package org.wildfly.plugins.demo.web.console;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
